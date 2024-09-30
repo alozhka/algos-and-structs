81532ebe33c5a45c4cf4d4b7308cfc514b78c8f3
Sergey <100339481+alozhka@users.noreply.github.com>
2024-09-30 16:42:27 +0300

    Создал lw3

---
+++ lw3/libs/TreeViewer.cpp
@@ -0,0 +1,9 @@
+//
+// Created by sergey on 9/30/24.
+//
+
+#include "TreeViewer.h"
+
+namespace Tree::Viewer
+{
+} // namespace Tree::Viewer
---
+++ lw3/libs/TreeViewer.h
@@ -0,0 +1,16 @@
+//
+// Created by sergey on 9/30/24.
+//
+
+#ifndef TREEVIEWER_H
+#define TREEVIEWER_H
+
+namespace Tree::Viewer
+{
+  class TreeViewer
+  {
+  };
+
+} // namespace Tree::Viewer
+
+#endif // TREEVIEWER_H
